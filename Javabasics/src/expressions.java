d57a4f7be9bc10a12099d88d54dd3370e827dd4b
Aditya Bhattad <adityabhattad18@gmail.com>
2023-04-25 17:57:09 +0530

    Expressions

---
+++ Javabasics/src/expressions.java
@@ -0,0 +1,12 @@
+public class expressions {
+    public static void main(String[] args) {
+        // Operands and Operators
+        // Operands = Value, Variables, Numbers and Quantity.
+        // Operators = + - * / %
+
+        int a = 10;
+        double b;
+        b=(double)a/3;
+        System.out.println("B is "+b);
+    }
+}
