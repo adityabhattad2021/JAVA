79b60678356b8f240df9c2552855e0d91d6e88b1
Aditya Bhattad <adityabhattad18@gmail.com>
2023-04-25 10:02:23 +0530

    Java Intro

--- helloWorldJAVA/src/App.java
+++ helloWorldJAVA/src/App.java
@@ -1,5 +1,14 @@
+// source code: Its a format where humans can easily read, when we compile source code to byte code it will generate a file with .class extension and that file can be run and executed in JVM(Java Virtual Machine).
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("\"Hello, World!\"");
+        System.out.               println("\"This will work as well\"");
     }
+
+    // This is a comment, same as C :)
+
+    /*
+     * 
+     * This is a multi line comment.
+     */
 }
