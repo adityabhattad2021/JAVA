422d6811e8367afb36e5eb0b18bc685f7e738325
Aditya Bhattad <adityabhattad18@gmail.com>
2023-05-22 14:11:44 +0530

    Problem 4

---
+++ practice-questions/src/problem4/package1/arithematic.java
@@ -0,0 +1,11 @@
+package problem4.package1;
+
+public class arithematic {
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ practice-questions/src/problem4/package2/packagedemo.java
@@ -0,0 +1,9 @@
+package problem4.package2;
+import problem4.package1.*;
+
+public class packagedemo {
+    public static void main(String[] args) {
+        System.out.printf("Addition of 2 and 3 is %d\n", arithematic.add(2, 3));
+        System.out.printf("Subtraction of 2 and 3 is %d\n", arithematic.sub(2, 3));
+    }
+}
