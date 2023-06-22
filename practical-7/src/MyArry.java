f19d7ff56c837b6c73f93c1ec27b3e26f25dcdd6
Aditya Bhattad <adityabhattad18@gmail.com>
2023-06-22 17:56:24 +0530

    Small fix

--- practical-7/src/MyArry.java
+++ practical-7/src/MyArry.java
@@ -11,28 +11,31 @@ public class MyArry<T> {
     }
 
     public void add(T element) {
-        if(this.arry.length<=size){
+        if (this.arry.length <= size) {
             this.arry[noofElement] = element;
             noofElement++;
-        }else{
+        } else {
             grow();
             add(element);
         }
     }
 
     private void grow() {
-        int newSize = (int) (this.size*1.5);
+        int newSize = (int) (this.size * 1.5);
         T[] newArry = (T[]) new Object[newSize];
         System.arraycopy(this.arry, 0, newArry, 0, this.size);
     }
 
-    public void swap(int index1,int index2) {
-        if(index1<0 || index1>this.arry.length || index2<0 || index2>this.arry.length){
-            try {
+    public void swap(int index1, int index2) {
+        try{
+            if (index1 < 0 || index1 > this.arry.length || index2 < 0 || index2 > this.arry.length) {
                 throw new Exception("Invalid indexes");
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+            T element = this.arry[index1];
+            this.arry[index1]=this.arry[index2];
+            this.arry[index2]=element;
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
@@ -40,7 +43,7 @@ public class MyArry<T> {
         return size;
     }
 
-    public T get(int index){
+    public T get(int index) {
         return this.arry[index];
     }
 
