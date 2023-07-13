880a48ff593af183bcf289ddac42818551194f6e
Aditya Bhattad <adityabhattad18@gmail.com>
2023-07-13 14:28:14 +0530

    object serialization example

--- serialization/src/App.java
+++ serialization/src/App.java
@@ -1,10 +1,26 @@
-/*
- *  
- */
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        FileOutputStream fileOutputStream = new FileOutputStream("user.ser", false);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        User user = new User();
+        user.setName("Aditya");
+
+        objectOutputStream.writeObject(user);
+        System.out.println(objectOutputStream.getClass());
+        objectOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("user.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        User userFromFile = (User) objectInputStream.readObject();
+        userFromFile.sayHello();
+
+        objectInputStream.close();
     }
 }
--- serialization/src/User.java
+++ serialization/src/User.java
@@ -2,12 +2,16 @@ import java.io.Serializable;
 
 
 public class User implements Serializable {
-    String name;
+    private String name;
 
     // Any thing with this keyword will be ignored in serialization process.
     transient String password;
 
     public void sayHello(){
         System.out.println(String.format("Hello %s",name));
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
